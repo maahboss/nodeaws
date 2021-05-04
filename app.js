27d2ff77997661ca309d2c811e132f135551fb8d
maahboss <31365719+maahboss@users.noreply.github.com>
2021-05-04 22:06:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const { PORT = 3000 } = process.env
 
 
 app.get('/', (req, res) => {
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server On');
 });
 
